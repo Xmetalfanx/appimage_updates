14d9e6023758df3db99498124ffcc8cb55d513be
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-07-18 15:22:44 -0400

    removed smarttube code from main .sh file

--- check_for_appimage_updates.sh
+++ check_for_appimage_updates.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 # config file
-. specifics/smarttube.cfg
 
-clear 
+clear
 
 
 function get_git_release_page() {
     echo "Getting Release page info"
     release_page="https://github.com/${repo_name}/releases"
 }
-
-
-
-check_smarttube_updates
