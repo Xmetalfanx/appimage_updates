85e23d3cbd2364b214fadca0db03eb2bf11e8c94
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-07-18 15:01:33 -0400

    Smarttube Stable and Beta latest version detection working

--- check_for_appimage_updates.sh
+++ check_for_appimage_updates.sh
@@ -3,6 +3,9 @@
 # config file
 . specifics/smarttube.cfg
 
+clear 
+
+
 function get_git_release_page() {
     echo "Getting Release page info"
     release_page="https://github.com/${repo_name}/releases"
