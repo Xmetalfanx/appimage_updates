71bb4236d6203497d8545325798251fd070ede94
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-07-19 15:58:49 -0400

    Updates to main .sh file for script

--- check_for_appimage_updates.sh
+++ check_for_appimage_updates.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # config file
+. configs/appimage.cfg
+. configs/specifics/appimage_apps.cfg
 
 clear
-
 
 function get_git_release_page() {
     echo "Getting Release page info"
     release_page="https://github.com/${repo_name}/releases"
 }
+
+bitwarden_check_update
+freetube_check_update
