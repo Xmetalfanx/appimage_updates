5de8b02bd594145c947133c00da508e6619f388c
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-07-30 15:34:28 -0400

    added userPrompt for debugging/pausing

--- check_for_appimage_updates.sh
+++ check_for_appimage_updates.sh
@@ -2,16 +2,23 @@
 
 # config file
 
-clear
+# clear
 . configs/appimage.cfg
 . configs/specifics/appimage_apps.cfg
 
+
+
+function userPrompt() {
+    read -r -p "Press [Enter] to continue "
+}
 
 function get_git_release_page() {
     echo "Getting Release page info"
     release_page="https://github.com/${repo_name}/releases"
 }
 
-bitwarden_check_update
-freetube_check_update
-ungoogle_chromium_update
+# bitwarden_check_update
+# freetube_check_update
+# ungoogle_chromium_update
+
+loop_through_appimages
