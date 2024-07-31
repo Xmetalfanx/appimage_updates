a96a44b204e8d41cf8d13e9533573f40e141f427
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-07-31 16:46:21 -0400

    cleanup

--- check_for_appimage_updates.sh
+++ check_for_appimage_updates.sh
@@ -14,10 +14,10 @@ function userPrompt() {
 function get_git_release_page() {
     echo "Getting Release page info"
     release_page="https://github.com/${repo_name}/releases"
+
+    echo -e "Release page for ${program_name} is: ${release_page}"
+    userPrompt
 }
 
-# bitwarden_check_update
-# freetube_check_update
-# ungoogle_chromium_update
 
 loop_through_appimages
